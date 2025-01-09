7de96a68cf2e8ea2e0bea50b518b63701fcd15fa
Hatooomlaski <alayakiadedamola@gmail.com>
2025-01-09 12:58:37 +0100

    fuction to seed users

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const DB = require('./Database/db');
-
+const bcrypt = require('bcrypt');
+const User = require('./model/userSchema');
 // Load environment variables
 dotenv.config();
 
@@ -27,3 +28,27 @@ app.use('/api/auth', authRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+
+async function seedUser  ()  {
+    try {
+        for (let i =0 ; i<51; i++) {
+            const password = `password${i}`;
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            const newUser = new User({
+                userName:`student${i}`,
+                password: hashedPassword,
+                email: `user${i}@yopmail.com`,
+                role: 'Student'
+        });
+console.log('Seeding users with password');
+        await newUser.save();
+    }
+    console.log("Users seeded successfully");
+    
+
+    } catch (error) {
+        console.log("Error while seeding", error);
+        
+    }
+} seedUser();
